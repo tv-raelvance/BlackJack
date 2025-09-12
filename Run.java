45a2e4d3865860ccbbe2d71e58b249bf71663185
MatthewJeffreyTV28 <mwjeffrey009@gmail.com>
2025-09-12 10:02:57 -0700

    finish

--- Run.java
+++ Run.java
@@ -1,6 +1,13 @@
+import java.util.ArrayList;
 public class Run {
     public static void main(String[] args) {
-       Prime.randomNum();
-        System.out.println(Prime.numbers.size());
+       Deck dew = new Deck();
+       ArrayList<Players> players = new ArrayList<>();
+
+       Player finnigyatt = new Player("finnigyatt");
+       Player Kunk = new Player("Kunk");
+       Player Graham = new Player("Graham");
+       Player Mitta_Fowler = new Player("Mitta Fowler");
+        
     }
 }
